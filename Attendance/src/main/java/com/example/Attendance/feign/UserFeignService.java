6b1957546c989903d9accccc5213030d275e81ce
jihyuk0414 <dealon2580@naver.com>
2025-02-07 01:33:53 +0900

    update : k8s 환경 배포로 변경 (feign)

--- Attendance/src/main/java/com/example/Attendance/feign/UserFeignService.java
+++ Attendance/src/main/java/com/example/Attendance/feign/UserFeignService.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 @Component
-@FeignClient(name = "User",url = "http://localhost:7070/user")
+//@FeignClient(name = "User",url = "http://localhost:7070/user")
+@FeignClient(name = "User",url = "http://user-service.default.svc.cluster.local:7070")
 public interface UserFeignService {
 
     @PostMapping("/employee/masking/list")
